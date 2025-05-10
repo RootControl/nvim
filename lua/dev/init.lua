1348241967b255ca4738d795912c4a1f338ccac7
RootControl <lh.root.control@gmail.com>
2025-05-10 19:14:41 -0300

    created nvim with lazyvim

---
+++ lua/dev/init.lua
@@ -0,0 +1,1 @@
+require("dev.config.lazy_init")
---
+++ lua/dev/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+	"folke/tokyonight.nvim",
+	-- make sure we load this during startup
+	-- if is your main colorscheme
+	lazy = false, 
+
+	-- make sure to load this
+	-- before all the other start plugins
+	opts = {
+	},
+}
