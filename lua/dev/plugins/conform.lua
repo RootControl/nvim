3bb70cfbd4b28027c89438263b91ea0d898a50e7
RootControl <lh.root.control@gmail.com>
2025-05-11 00:38:48 -0300

    added harpoon

--- lua/dev/plugins/conform.lua
+++ lua/dev/plugins/conform.lua
@@ -8,5 +8,13 @@ return {
                 go = { "gofmt" }
             }
         })
+
+        -- Use Conform to format the layout on save
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*",
+            callback = function(args)
+                require("conform").format({ bufnr = args.buf })
+            end,
+        })
     end
 }
