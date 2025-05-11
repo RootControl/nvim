ce698144e4f6338a9bab9efb93af7bde59f63baf
RootControl <lh.root.control@gmail.com>
2025-05-10 23:52:11 -0300

    added conform to format files on save

--- lua/dev/config/lazy_init.lua
+++ lua/dev/config/lazy_init.lua
@@ -38,3 +38,12 @@ require("lazy").setup({
 	-- automatically check for plugin updates
 	checker = { enable = true },
 })
+
+
+-- Use Conform to format the layout on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+    end,
+})
