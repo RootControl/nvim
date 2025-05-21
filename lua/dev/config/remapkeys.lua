f3ba24b4aad14cbd2d2855891845fda29f9eef66
RootControl <lh.root.control@gmail.com>
2025-05-21 20:19:15 -0300

    altered keymaps

--- lua/dev/config/remapkeys.lua
+++ lua/dev/config/remapkeys.lua
@@ -7,60 +7,75 @@ vim.keymap.set("n", "<leader><leader>", function()
 	vim.cmd("so")
 end)
 
-
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-
+vim.keymap.set("n", "<C-a>", "gg<S-v><S-g>", { desc = "select all" })
 
 -- view mode keymaps
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move select txt down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move select txt up" })
 
-
-
 -- golang custom keymaps
 vim.keymap.set(
-    "n",
-    "<leader>ee",
-    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>",
-    { desc = "If condition to check and return error" }
+	"n",
+	"<leader>ee",
+	"oif err != nil {<CR>}<Esc>Oreturn err<Esc>",
+	{ desc = "If condition to check and return error" }
 )
 
 vim.keymap.set(
-    "n",
-    "<leader>ef",
-    "oif err != nil {<CR>}<Esc>Olog.Fatalf(\"error: %s\\n\", err.Error())<Esc>jj",
-    { desc = "If condition to check and log.Fatal error" }
+	"n",
+	"<leader>ef",
+	'oif err != nil {<CR>}<Esc>Olog.Fatalf("error: %s\\n", err.Error())<Esc>jj',
+	{ desc = "If condition to check and log.Fatal error" }
 )
 
 vim.keymap.set(
-    "n",
-    "<leader>el",
-    "oif err != nil {<CR>}<Esc>O.logger.Error(\"error\", \"error\", err)<Esc>F.;i",
-    { desc = "If condition to check and return error" }
+	"n",
+	"<leader>el",
+	'oif err != nil {<CR>}<Esc>O.logger.Error("error", "error", err)<Esc>F.;i',
+	{ desc = "If condition to check and return error" }
 )
-
 
 -- LSP keymaps
-vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>kk", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    end
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(e)
+		local opts = { buffer = e.buf }
+		vim.keymap.set("n", "gd", function()
+			vim.lsp.buf.definition()
+		end, opts)
+		vim.keymap.set("n", "K", function()
+			vim.lsp.buf.hover()
+		end, opts)
+		vim.keymap.set("n", "<leader>vws", function()
+			vim.lsp.buf.workspace_symbol()
+		end, opts)
+		vim.keymap.set("n", "<leader>vd", function()
+			vim.diagnostic.open_float()
+		end, opts)
+		vim.keymap.set("n", "<leader>kk", function()
+			vim.lsp.buf.code_action()
+		end, opts)
+		vim.keymap.set("n", "<leader>vrr", function()
+			vim.lsp.buf.references()
+		end, opts)
+		vim.keymap.set("n", "<leader>vrn", function()
+			vim.lsp.buf.rename()
+		end, opts)
+		vim.keymap.set("i", "<C-h>", function()
+			vim.lsp.buf.signature_help()
+		end, opts)
+		vim.keymap.set("n", "[d", function()
+			vim.diagnostic.goto_next()
+		end, opts)
+		vim.keymap.set("n", "]d", function()
+			vim.diagnostic.goto_prev()
+		end, opts)
+	end,
 })
 
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
 })
-
